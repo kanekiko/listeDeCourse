50f6e8e226eac9582a352425ea302c7378d490c5
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-20 16:26:43 +0200

    feat: show alert if input is empty

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 'use strict'
+document.addEventListener('DOMContentLoaded', function () {
+  const pushButton = document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+      alert("Entrer un élément à ajouter à votre liste de course.");
+    }
+  }
+})
+
