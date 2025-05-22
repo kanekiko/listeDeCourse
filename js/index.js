bad8cc47fde06cb582d4d9f6f99fe8986b1d212f
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-22 10:35:40 +0200

    feat: else select input value

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const pushButton = document.querySelector('#push').onclick = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
       alert("Entrer un élément à ajouter à votre liste de course.");
+    } else {
+      document.querySelector('#tasks').innerHTML += `<div class="task"> <span id="taskname"> ${document.querySelector('#newtask input').value} </span> </div>`;
     }
   }
 })
