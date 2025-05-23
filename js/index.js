175a1153819623a2262f56c80826407ce672ec90
Sara Jourdan <sara.jourdanoff@icloud.com>
2025-05-23 10:12:02 +0200

    feat: replace document by window

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 'use strict'
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
   const pushButton = document.querySelector('#push').onclick = function () {
     if (document.querySelector('#newtask input').value.length == 0) {
       alert("Entrer un élément à ajouter à votre liste de course.");
